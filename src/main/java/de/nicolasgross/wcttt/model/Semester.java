e7fc6f337b0a1c8e960fee936fa1e28a8215a23b
Nicolas Gross <nicolas@fam-gross.org>
2018-06-13 23:57:36 +0900

    Add first bunch of classes

--- src/main/java/de/nicolasgross/wcttt/model/Semester.java
+++ src/main/java/de/nicolasgross/wcttt/model/Semester.java
@@ -1,4 +1,18 @@
 package de.nicolasgross.wcttt.model;
 
+import java.util.List;
+
 public class Semester {
+
+	private int daysPerWeek;
+	private int periodsPerDay;
+	private int minDailyLecturesPerCur;
+	private int maxDailyLecturesPerCur;
+
+	List<Teacher> teachers;
+	List<Room> rooms;
+	List<Course> courses;
+	List<Curriculum> curricula;
+	List<Timetable> timetables;
+
 }
