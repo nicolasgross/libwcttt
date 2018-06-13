a73cf3af06f2c0bdc9086e98db20b50dfd10c9d3
Nicolas Gross <nicolas@fam-gross.org>
2018-06-13 23:25:32 +0900

    Add project structure

---
+++ src/main/java/de/nicolasgross/wcttt/parser/Parser.java
@@ -0,0 +1,4 @@
+package de.nicolasgross.wcttt.parser;
+
+public class Parser {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module de.nicolasgross.wcttt.lib {
+	exports de.nicolasgross.wcttt.model;
+	exports de.nicolasgross.wcttt.parser;
+}
